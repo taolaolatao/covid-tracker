e44e4ea2c5e23a762d0a971e926a6a236c4fa734
VHM <taolaolataom@gmail.com>
2020-06-02 17:47:23 +0700

    Updates

---
+++ precache-manifest.037fba668e75b3294fdbbf52e0b4d6b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e9bfeec36b0db1877833ab737b09da",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "c0ea806c7c946e1afc48",
+    "url": "/covid-tracker/static/css/main.5c8fe8be.chunk.css"
+  },
+  {
+    "revision": "4462039c646803c6f8a8",
+    "url": "/covid-tracker/static/js/2.742459fd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.742459fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ea806c7c946e1afc48",
+    "url": "/covid-tracker/static/js/main.14ff445b.chunk.js"
+  },
+  {
+    "revision": "d18efbfbd5f9bcddd6ff",
+    "url": "/covid-tracker/static/js/runtime-main.fe48da39.js"
+  }
+]);
